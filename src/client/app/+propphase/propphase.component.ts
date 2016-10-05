e236c910e362fdb2fe3dc6aeaddb631cce4e2842
Berfu Taluy <b.taluy@hotmail.com>
2016-10-05 22:41:32 +0200

    made a start on prop phase

--- src/client/app/+propphase/propphase.component.ts
+++ src/client/app/+propphase/propphase.component.ts
@@ -14,6 +14,39 @@ export class PropPhaseComponent {
 
   public loading: boolean = false;
   public errorMessage: string;
+  public blok1Active: boolean = true;
+  public blok2Active: boolean = false;
+  public blok3Active: boolean = false;
+  public blok4Active: boolean = false;
 
   constructor(public route: ActivatedRoute, public router: Router) {}
+
+  activateBlok(blok: string) {
+    switch(blok) {
+      case 'blok1Active':
+        this.blok1Active = true;
+        this.blok2Active = false;
+        this.blok3Active = false;
+        this.blok4Active = false;
+      break;
+      case 'blok2Active':
+        this.blok1Active = false;
+        this.blok2Active = true;
+        this.blok3Active = false;
+        this.blok4Active = false;
+      break;
+      case 'blok3Active':
+        this.blok1Active = false;
+        this.blok2Active = false;
+        this.blok3Active = true;
+        this.blok4Active = false;
+      break;
+      case 'blok4Active':
+        this.blok1Active = false;
+        this.blok2Active = false;
+        this.blok3Active = false;
+        this.blok4Active = true;
+      break;
+    }
+  }
 }
