d5b412857dac534155a8d87951d02108437aaa56
Janine <janine@boeser.top>
2017-01-21 14:28:37 +0100

    Janine's magische code

--- src/app/+manage/+admin/+subjects/subjects.component.ts
+++ src/app/+manage/+admin/+subjects/subjects.component.ts
@@ -13,10 +13,34 @@ import { Modal } from 'angular2-modal/plugins/bootstrap';
   styleUrls: ['./subjects.component.scss']
 })
 export class SubjectsComponent {
-    public loading: boolean = false;
-    public errorMessage: string;
+  public loading: boolean = false;
+  public errorMessage: string;
+  public group1Active: boolean = true;
+  public group2Active: boolean = false;
+  public group3Active: boolean = false;
 
-    constructor(public apiService: ApiService, public route: ActivatedRoute, public overlay: Overlay, public vcRef: ViewContainerRef, public modal: Modal) {
-      overlay.defaultViewContainer = vcRef;
+  constructor(public apiService: ApiService, public route: ActivatedRoute, public overlay: Overlay, public vcRef: ViewContainerRef, public modal: Modal) {
+    overlay.defaultViewContainer = vcRef;
+  }
+
+  public activateGroupBlok(blok: string) {
+    switch (blok) {
+      case 'group1Active':
+        this.group1Active = true;
+        this.group2Active = false;
+        this.group3Active = false;
+        break;
+      case 'group2Active':
+        this.group1Active = false;
+        this.group2Active = true;
+        this.group3Active = false;
+        break;
+      case 'group3Active':
+        this.group1Active = false;
+        this.group2Active = false;
+        this.group3Active = true;
+        break;
     }
+  }
 }
+
--- src/app/+manage/+teacher/+grades/grades.component.ts
+++ src/app/+manage/+teacher/+grades/grades.component.ts
@@ -13,10 +13,34 @@ import { Modal } from 'angular2-modal/plugins/bootstrap';
   styleUrls: ['./grades.component.scss']
 })
 export class GradesComponent {
-    public loading: boolean = false;
-    public errorMessage: string;
+  public loading: boolean = false;
+  public errorMessage: string;
+  public gradeBlok1Active: boolean = true;
+  public gradeBlok2Active: boolean = false;
+  public gradeBlok3Active: boolean = false;
 
-    constructor(public apiService: ApiService, public route: ActivatedRoute, public overlay: Overlay, public vcRef: ViewContainerRef, public modal: Modal) {
-      overlay.defaultViewContainer = vcRef;
+  constructor(public apiService: ApiService, public route: ActivatedRoute, public overlay: Overlay, public vcRef: ViewContainerRef, public modal: Modal) {
+    overlay.defaultViewContainer = vcRef;
+  }
+
+  public activateGradeBlok(blok: string) {
+    switch (blok) {
+      case 'gradeBlok1Active':
+        this.gradeBlok1Active = true;
+        this.gradeBlok2Active = false;
+        this.gradeBlok3Active = false;
+        break;
+      case 'gradeBlok2Active':
+        this.gradeBlok1Active = false;
+        this.gradeBlok2Active = true;
+        this.gradeBlok3Active = false;
+        break;
+      case 'gradeBlok3Active':
+        this.gradeBlok1Active = false;
+        this.gradeBlok2Active = false;
+        this.gradeBlok3Active = true;
+        break;
     }
+  }
 }
+
